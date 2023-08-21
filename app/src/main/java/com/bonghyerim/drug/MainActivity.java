b0f5b4bd79b875599b71cdf10306aa022a11af59
bonghyerim <130967304+bonghyerim@users.noreply.github.com>
2023-08-21 12:10:09 +0900

    add

--- app/src/main/java/com/bonghyerim/drug/MainActivity.java
+++ app/src/main/java/com/bonghyerim/drug/MainActivity.java
@@ -1,9 +1,12 @@
 package com.bonghyerim.drug;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -12,6 +15,13 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.bonghyerim.drug.config.Config;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,6 +30,8 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
 
     TextView entpName, itemName, itemSeq, efcyQesitm, useMethodQesitm, atpnWarnQesitm, atpnQesitm, depositMethodQesitm;
+
+    ImageView itemImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         atpnWarnQesitm = findViewById(R.id.atpnWarnQesitm);
         atpnQesitm = findViewById(R.id.atpnQesitm);
         depositMethodQesitm = findViewById(R.id.depositMethodQesitm);
-
+        itemImage = findViewById(R.id.itemImage);
 
 
 
@@ -43,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
         JsonObjectRequest request = new JsonObjectRequest(
                 Request.Method.GET,
-                "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?serviceKey=eMeMZoqhFkwFUee4yNBwZg%2BoTNqM6rt2YV6KEeoHyE7h24m5mOCcizeY%2B25N94lnG7%2BNO0AadU2Bk62Xg7Z6lQ%3D%3D&pageNo=1&numOfRows=3&entpName=&itemName=정&type=json",
+                Config.HOST + Config.PATH + Config.API_KEY + "&pageNo=1&numOfRows=3&entpName=&itemName=" + "아스피린" + "&type=json",
                 null,
                 new Response.Listener<JSONObject>() {
                     @Override
@@ -52,22 +64,29 @@ public class MainActivity extends AppCompatActivity {
 
                         try {
 
-                            JSONArray items = response.getJSONObject("body").getJSONArray("items");
-                            String itemNameText = items.getJSONObject(0).getString("itemName");
+                            JSONObject body = response.getJSONObject("body");
+                            JSONArray items = body.getJSONArray("items");
+                            JSONObject firstItem = items.getJSONObject(0);
 
-                            String entpNameText  = items.getJSONObject(0).getString("entpName") ;
-                            int itemSeqText  = items.getJSONObject(0).getInt("itemSeq") ;
-                            String efcyQesitmText  = items.getJSONObject(0).getString("efcyQesitm") ;
-                            String useMethodQesitmText  = items.getJSONObject(0).getString("useMethodQesitm") ;
-                            String atpnWarnQesitmText  = items.getJSONObject(0).getString("atpnWarnQesitm") ;
-                            String atpnQesitmText  = items.getJSONObject(0).getString("atpnQesitm") ;
-                            String depositMethodQesitmText  = items.getJSONObject(0).getString("depositMethodQesitm") ;
+                            String itemNameText = firstItem.getString("itemName");
+                            String entpNameText = firstItem.getString("entpName");
+                            int itemSeqText = Integer.parseInt(firstItem.getString("itemSeq")); // 파싱한 문자열을 정수로 변환
+                            String efcyQesitmText = firstItem.getString("efcyQesitm");
+                            String useMethodQesitmText = firstItem.getString("useMethodQesitm");
+                            String atpnWarnQesitmText = firstItem.optString("atpnWarnQesitm", "정보없음"); // 기본값을 설정하여 null 처리
+                            String atpnQesitmText = firstItem.optString("atpnQesitm", "정보없음"); // 기본값을 설정하여 null 처리
+                            String depositMethodQesitmText = firstItem.getString("depositMethodQesitm");
+                            String itemImageUrl = firstItem.getString("itemImage");
+
+
+
 
                             itemName.setText(itemNameText+"");
                             entpName.setText(entpNameText+"");
                             itemSeq.setText(itemSeqText+"");
                             efcyQesitm.setText(efcyQesitmText+"");
                             useMethodQesitm.setText(useMethodQesitmText+"");
+
 
 
                             if (atpnWarnQesitmText != "null" && !atpnWarnQesitmText.isEmpty()) {
@@ -88,6 +107,12 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+                            Glide.with(MainActivity.this)
+                                    .load(itemImageUrl)
+                                    .placeholder(R.drawable.baseline_image_24)
+                                    .into(itemImage);
+
+
                         } catch (JSONException e) {
                             Log.i("TEST", "파싱에러");
                         }
@@ -105,5 +130,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
 
 }
